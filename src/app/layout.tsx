462d410b559802faf3a22fe1b568c367b57f55a6
Bruno BriSe <brise.bruno@gmail.com>
2025-12-23 10:59:49 +0100

    feat: implement smooth scrolling (Lenis) and noise texture overlay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import SmoothScroll from "./components/SmoothScroll";
+import NoiseOverlay from "./components/NoiseOverlay";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -74,7 +76,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SmoothScroll>
+          <NoiseOverlay />
+          {children}
+        </SmoothScroll>
       </body>
     </html>
   );
